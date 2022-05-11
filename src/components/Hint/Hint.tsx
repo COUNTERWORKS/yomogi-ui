a5c522db155ce86e001c6c82401db2c12f842731
Tsukuni <tamkchi.fugu@gmail.com>
2022-05-11 16:22:28 +0900

    add hint (#2)

---
+++ src/components/Hint/Hint.tsx
@@ -0,0 +1,17 @@
+import { FC, ComponentPropsWithRef } from 'react';
+import { Theme } from '../../themes';
+import { useTheme } from '../../hooks';
+import styled from '@emotion/styled';
+
+export const Hint: FC<ComponentPropsWithRef<'p'>> = ({ children, ...props }) => {
+  const theme = useTheme();
+
+  return <StyledHint theme={theme} {...props}>{children}</StyledHint>;
+};
+
+const StyledHint = styled.p<{theme: Theme }>`
+  display: block;
+  font-size: 12px;
+  color: ${({ theme }) => theme.colors.gray[400]};
+  margin: 4px 0;
+`;
