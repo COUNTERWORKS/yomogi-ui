852556c09f83dfade38fd4a600a109b2be5fc276
KentaTsukuni <tamkchi.fugu@gmail.com>
2022-07-14 16:22:05 +0900

    add checkbox storybook

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -1,7 +1,8 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Story, ComponentMeta } from '@storybook/react';
 import { FormBlock } from '../FormBlock';
 import { Checkbox } from './Checkbox';
 import { Flex } from '../Flex';
+import { Typograpy } from '../Typograpy';
 
 export default {
   title: 'YOMOGI-UI/Form/Checkbox',
@@ -9,15 +10,32 @@ export default {
 } as ComponentMeta<typeof Checkbox>;
 
 
-const Template: ComponentStory<typeof Checkbox> = () =>(
-  <FormBlock label='好きな食べ物' hint="hint">
-    <Flex>
-      <Checkbox id="apple" name="food" label="りんご" value="apple" />
-      <Checkbox id="orange" label="オレンジ" name="food" value="orange" />
-    </Flex>
-  </FormBlock>
-);
+const Template: Story = () => {
+  return (
+    <>
+      <div style={{ marginBottom: '24px' }}>
+        <Typograpy as="p" size="small">デフォルト</Typograpy>
+        <FormBlock label='好きな食べ物'>
+          <Flex>
+            <Checkbox id="apple" name="food" label="りんご" value="apple" />
+            <Checkbox id="orange" label="オレンジ" name="food" value="orange" />
+          </Flex>
+        </FormBlock>
+      </div>
+      <div style={{ marginBottom: '24px' }}>
+        <Typograpy as="p" size="small">エラー</Typograpy>
+        <FormBlock label='好きな食べ物' errorMessage='エラーが発生しました'>
+          <Flex>
+            <Checkbox id="apple-2" name="food-2" label="りんご" value="apple" />
+            <Checkbox id="orange-2" label="オレンジ" name="food-2" value="orange" />
+          </Flex>
+        </FormBlock>
+      </div>
+    </>
+  );
+};
 
-export const Default = Template.bind({});
+
+export const All = Template.bind({});
 
 
