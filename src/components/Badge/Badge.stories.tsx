c4899288caf719cd9dd39f479b79b7793b84d2d6
KentaTsukuni <tamkchi.fugu@gmail.com>
2022-07-14 12:35:32 +0900

    add badge storybook

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -1,70 +1,31 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Story, ComponentMeta } from '@storybook/react';
 import { Badge } from './Badge';
+import { Typograpy } from '../Typograpy';
+import { Flex } from '../Flex';
+
 
 export default {
   title: 'YOMOGI-UI/Badge',
   component: Badge,
-  args: {
-    color: 'primary',
-    children: 'badge',
-  },
-  argTypes: {
-    color: {
-      options: ['primary', 'secondary', 'neutral', 'light', 'info', 'success', 'caution', 'error'],
-      control: {
-        type: 'radio',
-      },
-    },
-  },
 } as ComponentMeta<typeof Badge>;
 
 
-const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />;
-
-export const Primary = Template.bind({});
-
-Primary.args = {
-  color: 'primary',
+const Template: Story = () => {
+  return (
+    <div>
+      <Typograpy as="h3" size="small">色一覧</Typograpy>
+      <Flex gap={1}>
+        <Badge color="primary">primary</Badge>
+        <Badge color="secondary">secondary</Badge>
+        <Badge color="info">info</Badge>
+        <Badge color="success">success</Badge>
+        <Badge color="caution">caution</Badge>
+        <Badge color="error">error</Badge>
+        <Badge color="neutral">neutral</Badge>
+        <Badge color="light">light</Badge>
+      </Flex>
+    </div>
+  );
 };
 
-export const Secondary = Template.bind({});
-
-Secondary.args = {
-  color: 'secondary',
-};
-
-export const Info = Template.bind({});
-
-Info.args = {
-  color: 'info',
-};
-
-export const Success = Template.bind({});
-
-Success.args = {
-  color: 'success',
-};
-
-export const Caution = Template.bind({});
-
-Caution.args = {
-  color: 'caution',
-};
-
-export const Error = Template.bind({});
-
-Error.args = {
-  color: 'error',
-};
-
-export const Neutral = Template.bind({});
-
-Neutral.args = {
-  color: 'neutral',
-};
-
-export const Light = Template.bind({});
-
-Light.args = {
-  color: 'light',
-};
+export const All = Template.bind({});
