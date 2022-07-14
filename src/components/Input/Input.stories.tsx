259db9da983566f98559bf8c0e1192c7fd19ee87
KentaTsukuni <tamkchi.fugu@gmail.com>
2022-07-14 16:33:18 +0900

    fix label

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -16,14 +16,21 @@ const Template: Story = () => {
         <Typograpy as="p" size="small">デフォルト</Typograpy>
         <FormBlock label='名前'><Input /></FormBlock>
         <FormBlock label='email' hint="メールアドレスを入力してください"><Input /></FormBlock>
-        <FormBlock label='email' hint="8文字以上で入力してください"><Input type="password"/></FormBlock>
+        <FormBlock label='password' hint="8文字以上で入力してください"><Input type="password"/></FormBlock>
       </div>
 
       <div style={{ marginBottom: '24px' }}>
         <Typograpy as="p" size="small">エラー</Typograpy>
         <FormBlock label='名前' errorMessage='入力してください'><Input isError /></FormBlock>
         <FormBlock label='email' hint="メールアドレスを入力してください" errorMessage='入力してください'><Input isError/></FormBlock>
-        <FormBlock label='email' hint="8文字以上で入力してください" errorMessage='入力してください'><Input type="password" isError/></FormBlock>
+        <FormBlock label='password' hint="8文字以上で入力してください" errorMessage='入力してください'><Input type="password" isError/></FormBlock>
+      </div>
+
+      <div style={{ marginBottom: '24px' }}>
+        <Typograpy as="p" size="small">disabled</Typograpy>
+        <FormBlock label='名前'><Input disabled /></FormBlock>
+        <FormBlock label='email' hint="メールアドレスを入力してください"><Input disabled /></FormBlock>
+        <FormBlock label='password' hint="8文字以上で入力してください"><Input type="password" disabled /></FormBlock>
       </div>
 
     </>
