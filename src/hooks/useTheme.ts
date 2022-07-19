bd352ed5694590f0c73706c9f0853df04972e057
Tsukuni <tamkchi.fugu@gmail.com>
2022-07-19 14:45:33 +0900

    first release (#24)

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,8 +1,8 @@
 import React from 'react';
-import { ThemeContext } from '../components/ThemeProvider';
+import { YomogiThemeContext } from '../components/ThemeProvider';
 
 export const useTheme = () => {
-  const theme = React.useContext(ThemeContext);
+  const theme = React.useContext(YomogiThemeContext);
 
   return theme;
 };
