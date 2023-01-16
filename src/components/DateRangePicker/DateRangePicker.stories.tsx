3a6b4f4570b015fe385b35de2fefcab1293de328
Tsukuni <tamkchi.fugu@gmail.com>
2023-01-16 16:57:41 +0900

    datepicker追加 (#35)
    * datepicker追加
    
    * fix design
    
    * remove background
    
    * 0.0.16
    
    Co-authored-by: kentymmt <kent.ymmt@gmail.com>

---
+++ src/components/DateRangePicker/DateRangePicker.stories.tsx
@@ -0,0 +1,87 @@
+import { Story, ComponentMeta } from '@storybook/react';
+import { DateRangePicker } from './DateRangePicker';
+import { Typography } from '../Typography';
+import { Input } from '../Input';
+import { Flex } from '../Flex';
+import { FormBlock } from '../FormBlock';
+
+export default {
+  title: 'YOMOGI-UI/DateRangePicker',
+  component: DateRangePicker,
+} as ComponentMeta<typeof DateRangePicker>;
+
+const Template: Story = () => {
+  return (
+    <>
+      <div style={{ marginBottom: '24px' }}>
+        <FormBlock>
+          <Typography as="p" size="small">
+            デフォルト
+          </Typography>
+          <DateRangePicker
+            renderInput={({ startProps, endProps }) => (
+              <Flex gap={4}>
+                <Input {...startProps} />
+                <Input {...endProps} />
+              </Flex>
+            )}
+          />
+        </FormBlock>
+        <FormBlock>
+          <Typography as="p" size="small">
+            ２ヶ月表示ver
+          </Typography>
+          <DateRangePicker
+            numberOfMonths={2}
+            renderInput={({ startProps, endProps }) => (
+              <Flex gap={4}>
+                <Input {...startProps} />
+                <Input {...endProps} />
+              </Flex>
+            )}
+          />
+        </FormBlock>
+        <FormBlock>
+          <Typography as="p" size="small">
+            初期値あり
+          </Typography>
+          <DateRangePicker
+            defaultStartDate="2022/01/05"
+            defaultEndDate="2022/01/10"
+            renderInput={({ startProps, endProps }) => (
+              <Flex gap={4}>
+                <Input {...startProps} />
+                <Input {...endProps} />
+              </Flex>
+            )}
+          />
+        </FormBlock>
+
+        <FormBlock>
+          <Typography as="p" size="small">
+            noMargin
+          </Typography>
+          <DateRangePicker
+            noMargin
+            noBorder
+            renderInput={({ startProps, endProps }) => (
+              <Flex gap={4}>
+                <Input {...startProps} />
+                <Input {...endProps} />
+              </Flex>
+            )}
+          />
+        </FormBlock>
+
+        <FormBlock>
+          <Typography as="p" size="small">
+            default open
+          </Typography>
+          <DateRangePicker noMargin noBorder opened renderInput={() => <></>} absolute={false} />
+        </FormBlock>
+      </div>
+    </>
+  );
+};
+
+export const All = Template.bind({});
---
+++ src/components/DateRangePicker/util.ts
@@ -0,0 +1,112 @@
+import { lastDayOfMonth, startOfMonth, isSameDay, isAfter, isBefore } from 'date-fns';
+import { format } from 'date-fns';
+import { CellType } from './types';
+
+export const getDaysInMonth = (date: Date): Array<Partial<number>> => {
+  const lastDate = lastDayOfMonth(date).getDate();
+  const startDay = startOfMonth(date).getDay();
+
+  const dates = Array(lastDate)
+    .fill(null)
+    .map((_, index: number) => index + 1);
+  const emptyArray = Array(startDay).fill(null);
+
+  return [...emptyArray, ...dates];
+};
+
+export const getCellType = (dates: { startDate: string; endDate: string }, date: Date): CellType => {
+  const startDate = isValidDateFormat(dates.startDate) ? toDate(dates.startDate) : '';
+  const endDate = isValidDateFormat(dates.endDate) ? toDate(dates.endDate) : '';
+
+  if (!endDate && !startDate) {
+    return 'default';
+  }
+
+  if (endDate && startDate) {
+    if (isSameDay(endDate, startDate) && isSameDay(startDate, date)) {
+      return 'single';
+    }
+
+    if (isSameDay(startDate, date)) {
+      return 'start';
+    }
+
+    if (isSameDay(endDate, date)) {
+      return 'end';
+    }
+
+    if (isAfter(endDate, date) && isBefore(startDate, date)) {
+      return 'middle';
+    }
+  } else {
+    if (startDate && isSameDay(startDate, date)) {
+      return 'start';
+    }
+
+    if (endDate && isSameDay(endDate, date)) {
+      return 'end';
+    }
+  }
+
+  return 'default';
+};
+
+// 選択期間に利用不可日がないかを調べる関数
+export const hasUnavailableDatesInSelectedDates = ({
+  unavailableDates,
+  startDate,
+  endDate,
+}: {
+  unavailableDates: string[];
+  startDate: string;
+  endDate: string;
+}): boolean => {
+  const date = unavailableDates.find(
+    (date) => !isBefore(new Date(date), toDate(startDate)) && !isAfter(new Date(date), toDate(endDate))
+  );
+
+  return Boolean(date);
+};
+
+export const isDisabled = ({
+  unavailableDates,
+  startDate,
+  endDate,
+}: {
+  unavailableDates: string[];
+  startDate: string;
+  endDate: string;
+}): boolean => {
+  if (!startDate || !endDate) return false;
+
+  return hasUnavailableDatesInSelectedDates({ startDate, endDate, unavailableDates });
+};
+
+export const isUnavailable = (
+  unavailableDates: Array<string>,
+  date: Date,
+  minDate?: string,
+  maxDate?: string
+): boolean => {
+  if (minDate && isValidDateFormat(minDate) && isAfter(toDate(minDate), date)) {
+    return true;
+  }
+  if (maxDate && isValidDateFormat(maxDate) && isBefore(toDate(maxDate), date)) {
+    return true;
+  }
+
+  return unavailableDates.some((unavailableDate: string) => isSameDay(new Date(unavailableDate), date));
+};
+
+export const isValidDateFormat = (value: string) => {
+  const regexp = new RegExp(/^[0-9]{4}\/(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])$/);
+  return regexp.test(value);
+};
+
+export const toDate = (value: string) => {
+  return new Date(value);
+};
+
+export const formatDate = (date: Date) => {
+  return format(date, 'yyyy/MM/dd');
+};
