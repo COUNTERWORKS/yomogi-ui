0e74667e3de5d0e60d9bbd770c66ba197ab9d62c
WataruOno39 <wataru39ono@gmail.com>
2023-02-07 14:53:13 +0900

    無駄なスクロールバーが出ないようにした

--- src/components/Modal/ModalBody.tsx
+++ src/components/Modal/ModalBody.tsx
@@ -4,16 +4,13 @@ import styled from '@emotion/styled';
 type Props = {
   children: React.ReactNode;
   className?: string;
-}
+};
 
 export const ModalBody: React.FC<Props> = ({ children, className }) => {
-  return (
-    <Container className={className}>{children}</Container>
-  );
+  return <Container className={className}>{children}</Container>;
 };
 
 const Container = styled.div`
   padding: 16px 24px;
   box-sizing: border-box;
-  overflow: scroll;
 `;
